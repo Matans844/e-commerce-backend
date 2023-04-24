7447ed844192a4c7aafe62fe97b607f71bb2860d
Lord-Whitesmith <matans844@gmail.com>
2023-04-24 21:35:53 +0300

    Testing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,14 +3,16 @@ import morgan from 'morgan';
 import cors from 'cors';
 import helmet from 'helmet';
 import compression from 'compression';
+import { config } from 'dotenv';
+import connectDB from '../configurations/database';
+/**
 import { accountRoutes } from './backend/src/routes/accountRoutes';
 import { authRoutes } from './backend/src/routes/authRoutes';
 import { productRoutes } from './backend/src/routes/productRoutes';
 import { cartRoutes } from './backend/src/routes/cartRoutes';
 import { checkoutRoutes } from './backend/src/routes/checkoutRoutes';
 import { errorHandler } from './backend/src/utils/errorHandler';
-import connectDB from '../configurations/database';
-import { config } from 'dotenv';
+ */
 
 config();
 
@@ -20,6 +22,10 @@ const port = process.env['PORT'] || 5000;
 // Connect to database
 await connectDB();
 
+server.get("/", (_, res) => {
+    res.send("API IS RUNNING...");
+});
+
 // Middleware
 server.use(express.json());
 server.use(morgan('dev'));
@@ -28,14 +34,18 @@ server.use(helmet());
 server.use(compression());
 
 // API routes
+/**
 server.use('/api/v1/accounts', accountRoutes);
 server.use('/api/v1/auth', authRoutes);
 server.use('/api/v1/products', productRoutes);
 server.use('/api/v1/carts', cartRoutes);
 server.use('/api/v1/checkout', checkoutRoutes);
+*/
 
 // Error handling middleware
+/**
 server.use(errorHandler);
+ */
 
 server.listen(port, () => {
     console.log(`Server listening on port ${port}`);
