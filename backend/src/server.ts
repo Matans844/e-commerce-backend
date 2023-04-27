c97407d0ab1ceb63d112caba325a4660ba19f422
Matans844 <matans844@gmail.com>
2023-04-27 20:19:47 +0300

    Simplified packages. Before testing.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,14 +4,15 @@ import connectDB from '../configurations/database.js';
 
 // Middlewares
 import morgan from 'morgan';
-import { errorHandler } from './backend/src/utils/errorHandler';
+/*import { errorHandler } from './backend/src/utils/errorHandler';*/
 
 // Routes
-import { accountRoutes } from './backend/src/routes/accountRoutes';
+/*import { accountRoutes } from './backend/src/routes/accountRoutes';
 import { authRoutes } from './backend/src/routes/authRoutes';
 import { productRoutes } from './backend/src/routes/productRoutes';
 import { cartRoutes } from './backend/src/routes/cartRoutes';
-import { checkoutRoutes } from './backend/src/routes/checkoutRoutes';
+import { checkoutRoutes } from './backend/src/routes/checkoutRoutes';*/
+
 
 dotenv.config();
 
@@ -19,21 +20,21 @@ const server = express();
 const port = process.env['PORT'] || 3000;
 
 if (process.env['NODE_ENV'] === 'development') {
-    app.use(morgan('dev'))
+    server.use(morgan('dev'))
 }
 
 // Middleware
 server.use(express.json());
 
 // API routes
-server.use('/api/v1/accounts', accountRoutes);
+/*server.use('/api/v1/accounts', accountRoutes);
 server.use('/api/v1/auth', authRoutes);
 server.use('/api/v1/products', productRoutes);
 server.use('/api/v1/carts', cartRoutes);
-server.use('/api/v1/checkout', checkoutRoutes);
+server.use('/api/v1/checkout', checkoutRoutes);*/
 
 // Error handling middleware
-server.use(errorHandler);
+/*server.use(errorHandler);*/
 
 async function startServer() {
     // Connect to database
