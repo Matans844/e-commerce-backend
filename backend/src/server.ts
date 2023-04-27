9200575870efb06f051c16911e47836a58079c75
Matans844 <matans844@gmail.com>
2023-04-27 15:14:32 +0300

    Design changes: 1. Error middleware takes care of errors rising from the routes, which, in turn, capture business layer logic. 2. Removing redundant middlewares. 3. Stacking middlewares.
    In the orchestrator:
    1. Simplification: Removed middlewares like cors or helmet.
    2. Placed error handler last, so that route errors propagate down to it.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,19 +1,13 @@
 import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-import helmet from 'helmet';
-import compression from 'compression';
 import { config } from 'dotenv';
 import connectDB from '../configurations/database.js';
-/**
-import { corsMiddleware } from './backend/src/middleware/corsMiddleware';
+import morgan from 'morgan';
 import { accountRoutes } from './backend/src/routes/accountRoutes';
 import { authRoutes } from './backend/src/routes/authRoutes';
 import { productRoutes } from './backend/src/routes/productRoutes';
 import { cartRoutes } from './backend/src/routes/cartRoutes';
 import { checkoutRoutes } from './backend/src/routes/checkoutRoutes';
 import { errorHandler } from './backend/src/utils/errorHandler';
- */
 
 config();
 
@@ -21,28 +15,18 @@ const server = express();
 const port = process.env['PORT'] || 3000;
 
 // Middleware
-/**
-server.use(corsMiddleware);
 server.use(express.json());
 server.use(morgan('dev'));
-server.use(cors());
-server.use(helmet());
-server.use(compression());
-*/
 
 // API routes
-/**
 server.use('/api/v1/accounts', accountRoutes);
 server.use('/api/v1/auth', authRoutes);
 server.use('/api/v1/products', productRoutes);
 server.use('/api/v1/carts', cartRoutes);
 server.use('/api/v1/checkout', checkoutRoutes);
-*/
 
 // Error handling middleware
-/**
 server.use(errorHandler);
- */
 
 async function startServer() {
     // Connect to database
