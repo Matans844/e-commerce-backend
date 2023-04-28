5a24f22ccb9c6cf7c456450f9f09e51821b88470
Matans844 <matans844@gmail.com>
2023-04-28 03:55:36 +0300

    Finished Product Model for schema. This is used to query the database. The schema does not support adding quotas to products, or authenticated users that uploaded a product.

--- backend/src/models/ProductModel.ts
+++ backend/src/models/ProductModel.ts
@@ -1,29 +1,6 @@
 import { model, Schema } from "mongoose";
 import { ProductDocument } from "../types/Product.js";
 
-const reviewSchema = new Schema(
-    {
-        user: {
-            type: Schema.Types.ObjectId,
-            required: true,
-            ref: "User",
-        },
-        name: {
-            type: String,
-            required: true,
-        },
-        rating: {
-            type: Number,
-            required: true,
-        },
-        comment: {
-            type: String,
-            required: true,
-        },
-    },
-    { timestamps: true }
-);
-
 const productSchema = new Schema(
     {
         user: {
@@ -35,42 +12,14 @@ const productSchema = new Schema(
             type: String,
             required: true,
         },
-        image: {
-            type: String,
-            required: true,
-        },
-        brand: {
-            type: String,
-            required: true,
-        },
-        category: {
-            type: String,
-            required: true,
-        },
         description: {
             type: String,
             required: true,
         },
-        reviews: [reviewSchema],
-        rating: {
-            type: Number,
-            required: true,
-            default: 0,
-        },
-        numReviews: {
-            type: Number,
-            required: true,
-            default: 0,
-        },
         price: {
             type: Number,
             required: true,
-            default: 0,
-        },
-        countInStock: {
-            type: Number,
-            required: true,
-            default: 0,
+            default: 0.0,
         },
     },
     {
