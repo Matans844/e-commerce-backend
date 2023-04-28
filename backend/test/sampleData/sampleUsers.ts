7127b8b4dba94594246da34ce8a39bdad39e87b1
Matans844 <matans844@gmail.com>
2023-04-28 21:24:05 +0300

    Added sample data users.

---
+++ backend/test/sampleData/sampleUsers.ts
@@ -0,0 +1,27 @@
+import bcrypt from 'bcryptjs'
+import { type User } from '../../src/types/index.js'
+
+const user1: User = {
+  name: 'John Snow',
+  email: 'john@example.com',
+  password: bcrypt.hashSync('123456', 10),
+  address: 'Herzliya'
+}
+
+const user2: User = {
+  name: 'Jane Doe',
+  email: 'Jane@Doe.com',
+  password: bcrypt.hashSync('123456', 10),
+  address: 'Kfar Saba'
+}
+
+const user3: User = {
+  name: 'Test User',
+  email: 'test@test.com',
+  password: bcrypt.hashSync('123456', 10),
+  address: 'North Pole'
+}
+
+const users: any[] = [user1, user2, user3]
+
+export default users
