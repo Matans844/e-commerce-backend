62865ff7d710617daedfb245c6fc35781914f95a
Matans844 <matans844@gmail.com>
2023-04-30 00:57:46 +0300

    Added error middleware

---
+++ backend/src/middleware/errorMiddleware.ts
@@ -0,0 +1,23 @@
+import { type NextFunction, type Request, type Response } from 'express'
+
+const notFound = (req: Request, res: Response, next: NextFunction): void => {
+  const error = new Error(`Not found - ${req.originalUrl}`)
+  res.status(404)
+  next(error)
+}
+
+const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+  res.status(statusCode)
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack
+  })
+}
+
+export { notFound, errorHandler }
